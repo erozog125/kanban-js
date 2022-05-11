b0c8ebdd2548aa39d79d16c08495858050753a13
Edwin Rozo Gómez <erozog125@gmail.com>
2022-05-10 22:28:40 -0500

    Agregando funcionalidades

--- app.js
+++ app.js
@@ -6,14 +6,23 @@ const title = document.getElementById('input-title');
 const description = document.getElementById('ta-description');
 const priority = document.getElementById('select-priority');
 
+
 const generate = document.getElementById('generate');
 
-generate.addEventListener('click', GenerateCard);
+generate.addEventListener('click', renderTask);
 
-function GenerateCard() {
+function renderTask() {
+    GenerateCard();
+}
 
+
+function GenerateCard(board = todo) {
+    
     const card = document.createElement('div');
     card.classList.add('card-task');
+    const btnClose = document.createElement('button');
+    btnClose.classList.add('btn-close');
+    btnClose.textContent = 'X';    
     const titleList = document.createElement('h2');
     titleList.textContent = title.value;
     const descriptionList = document.createElement('p');
@@ -21,33 +30,56 @@ function GenerateCard() {
     const priorityList = document.createElement('p');
     priorityList.textContent = priority.value;
     const selectList = document.createElement('select');
+    selectList.classList.add('select-list');
     const optToDo = document.createElement('option');
     const optMove = document.createElement('option');
     const optDoing = document.createElement('option');
     const optDone = document.createElement('option');
 
     optMove.value = "Move";
-    optToDo.value = "todo";
-    optDoing.value = "doing";
-    optDone.value = "done";
+    optToDo.value = "ToDo";
+    optDoing.value = "Doing";
+    optDone.value = "Done";
 
     optMove.textContent = "Move";
-    optToDo.textContent = "todo";
-    optDoing.textContent = "doing";
-    optDone.textContent = "done";
-
+    optToDo.textContent = "ToDo";
+    optDoing.textContent = "Doing";
+    optDone.textContent = "Done";
 
     selectList.appendChild(optMove);
     selectList.appendChild(optToDo);
     selectList.appendChild(optDoing);
     selectList.appendChild(optDone);
 
+    card.appendChild(btnClose);
     card.appendChild(titleList);
     card.appendChild(descriptionList);
     card.appendChild(priorityList);
     card.appendChild(selectList);
 
-    todo.appendChild(card);
+    board.appendChild(card);
+
+    const close = document.querySelector('.btn-close');
+    close.addEventListener('click', closeCard);
+    const selectMove = document.querySelector('.select-list');
+    selectMove.addEventListener('change', moveCard);
+}
+
+function moveCard(event) {
+    
+    const selectCard = event.target.value;    
+    
+    if (selectCard === "ToDo") {
+        GenerateCard();
+    } else if (selectCard === "Doing") {
+        GenerateCard(doing);
+    } else {
+        GenerateCard(done);
+    }
+}
+
+function closeCard(event) {
+    event.target.parentElement.remove();
 }
 
 
